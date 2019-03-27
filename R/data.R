f9d17e4d211106493f3c322edd09f2377fca0f06
Loïc Lannelongue <l.lannelongue@gmail.com>
2019-03-27 19:24:37 +0000

    Add details to documentation. Add the servo dataset.

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Data from the servo dataset
+#'
+#' [description of servo]
+#'
+#' @format A data frame with .. rows and .. variables:
+#' \describe{
+#'   \item{v1}{blabla}
+#'   \item{v2}{}
+#'   ...
+#' }
+#' @source \url{https://archive.ics.uci.edu/ml/datasets/Servo}
+"servo"
